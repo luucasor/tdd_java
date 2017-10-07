0c6c09bc86ad0e35d1430ed76495b12aad7753bd
luucasor <lucas@gati.net.br>
2017-10-07 19:31:33 -0300

    Adicionado import statico da classe Assert

--- Test/br/com/caelum/leilao/dominio/PalindromoTest.java
+++ Test/br/com/caelum/leilao/dominio/PalindromoTest.java
@@ -1,6 +1,6 @@
 package br.com.caelum.leilao.dominio;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PalindromoTest {
@@ -8,18 +8,18 @@ public class PalindromoTest {
     @Test
     public void validaSeAFraseOnibusMarrocosEhPalindromo(){
         final String onibusMarrocos = "Socorram-me subi no onibus em Marrocos";
-        Assert.assertEquals(true, new Palindromo().ehPalindromo(onibusMarrocos));
+        assertEquals(true, new Palindromo().ehPalindromo(onibusMarrocos));
     }
 
     @Test
     public void validaSeAFraseAnotaramDataMaratonaEhPalindromo(){
         final String anotaramDataMaratona = "Anotaram a data da maratona";
-        Assert.assertEquals(true, new Palindromo().ehPalindromo(anotaramDataMaratona));
+        assertEquals(true, new Palindromo().ehPalindromo(anotaramDataMaratona));
     }
 
     @Test
     public void validaSeAFraseAleatoriaEhPalindromo() {
         final String fraseAleatoria = "Escrita de frase aleatória";
-        Assert.assertFalse(new Palindromo().ehPalindromo(fraseAleatoria));
+        assertFalse(new Palindromo().ehPalindromo(fraseAleatoria));
     }
 }
