79e838e2af878e5b831dc03a21d1de81953b4e61
luucasor <lucas@gati.net.br>
2017-10-08 21:18:45 -0300

    Classe builder para criacao do leilao

--- src/br/com/caelum/leilao/dominio/CriadorDeLeilao.java
+++ src/br/com/caelum/leilao/dominio/CriadorDeLeilao.java
@@ -1,4 +1,21 @@
 package br.com.caelum.leilao.dominio;
 
 public class CriadorDeLeilao {
+    private Leilao leilao;
+
+    public CriadorDeLeilao() { }
+
+    public CriadorDeLeilao para(String descricao) {
+        this.leilao = new Leilao(descricao);
+        return this;
+    }
+
+    public CriadorDeLeilao lance(Usuario usuario, double valor) {
+        leilao.propoe(new Lance(usuario, valor));
+        return this;
+    }
+
+    public Leilao constroi() {
+        return leilao;
+    }
 }
